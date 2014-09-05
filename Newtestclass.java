05f57b1b4d2b5a308ea387a1ebf73884ba1636c0
davit-gh <davsmile@yahoo.com>
2014-09-05 15:12:08 +0400

    test commit 4

--- Newtestclass.java
+++ Newtestclass.java
@@ -13,7 +13,8 @@ public class Newtestclass {
     public void setWebDriver(){
         WebDriver driver = new FirefoxDriver();
         driver.navigate().to("http://seleniumsimplified.com");
-        Assert.assertTrue("Should be different", driver.getTitle().startsWith("Selenium Simplified"));
+        Assert.assertTrue("Should be different",
+                driver.getTitle().startsWith("Selenium Simplified"));
         driver.close();
 
     }
